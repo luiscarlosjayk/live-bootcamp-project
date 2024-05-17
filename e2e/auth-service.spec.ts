b2c169b747395acbb854dd7c0b26fea62050e2a1
Luis Carlos Osorio Jayk <luis.osorio@wizeline.com>
2024-05-17 12:27:41 -0600

    feat: setup everything to run end to end tests

---
+++ e2e/auth-service.spec.ts
@@ -0,0 +1,24 @@
+import { test, expect, type Page } from '@playwright/test';
+
+const basePath = '/auth';
+
+test.beforeEach(async ({ page }) => {
+    await page.goto(basePath);
+});
+
+test.describe('Login', () => {
+    test('should login form', async ({ page }) => {
+        const emailField = page.getByRole('textbox', { name: 'Email' });
+        const passwordField = page.getByRole('textbox', { name: 'Password' });
+        const submitButton = page.getByRole('button', { name: 'Log in' });
+
+        await emailField.fill('admin@test.com');
+        await passwordField.fill('123456');
+
+        const loginRequestPromise = page.waitForResponse(basePath + '/login');
+        await submitButton.click();
+        const loginRequestResponse = await loginRequestPromise;
+
+        expect(loginRequestResponse.status()).toBe(200);
+    });
+});
