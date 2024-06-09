d793726b844d7c5a461a6cc0641d79c9642bbec4
Luis Carlos Osorio Jayk <luis.osorio@wizeline.com>
2024-06-09 10:13:01 -0600

    ci: disable compilation of protobuffers in build files

--- app-service/src/bin/grpc_client.rs
+++ app-service/src/bin/grpc_client.rs
@@ -1,24 +1,24 @@
-pub mod proto {
-    tonic::include_proto!("authentication");
-}
-use dotenvy::dotenv;
-use proto::auth_client::AuthClient;
-use proto::VerifyTokenRequest;
+// pub mod proto {
+//     tonic::include_proto!("authentication");
+// }
+// use dotenvy::dotenv;
+// use proto::auth_client::AuthClient;
+// use proto::VerifyTokenRequest;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    dotenv().ok();
+    // dotenv().ok();
 
-    let auth_ip = "[::0]".to_string();
-    let mut client = AuthClient::connect(format!("http://{}:50051", auth_ip)).await?;
+    // let auth_ip = "[::0]".to_string();
+    // let mut client = AuthClient::connect(format!("http://{}:50051", auth_ip)).await?;
 
-    let token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ0ZXN0QHRlc3QuY29tIiwiZXhwIjoxNzE3ODI5ODI5fQ.KD7y0DBVZP5oacOsn2BS-8aFxe_Ys2phCcQu3-3FQ24";
-    let request = tonic::Request::new(VerifyTokenRequest {
-        token: token.into(),
-    });
-    let response = client.verify_token(request).await?.into_inner();
+    // let token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ0ZXN0QHRlc3QuY29tIiwiZXhwIjoxNzE3ODI5ODI5fQ.KD7y0DBVZP5oacOsn2BS-8aFxe_Ys2phCcQu3-3FQ24";
+    // let request = tonic::Request::new(VerifyTokenRequest {
+    //     token: token.into(),
+    // });
+    // let response = client.verify_token(request).await?.into_inner();
 
-    dbg!(response);
+    // dbg!(response);
 
     Ok(())
 }
--- auth-service/src/bin/grpc_server.rs
+++ auth-service/src/bin/grpc_server.rs
@@ -1,21 +1,23 @@
-use auth_service::services::{grpc_auth, hashset_banned_token_store::HashsetBannedTokenStore};
-use dotenvy::dotenv;
-use std::sync::Arc;
-use tokio::sync::RwLock;
+// use auth_service::services::{grpc_auth, hashset_banned_token_store::HashsetBannedTokenStore};
+// use dotenvy::dotenv;
+// use std::sync::Arc;
+// use tokio::sync::RwLock;
 
 #[tokio::main]
 async fn main() -> Result<(), tonic::transport::Error> {
-    dotenv().ok();
+    // dotenv().ok();
 
-    let addr = "[::0]:50051"
-        .parse()
-        .map_err(|_| tonic::Status::internal("Could not parse grpc address"));
-    let banned_token_store = Arc::new(RwLock::new(HashsetBannedTokenStore::default()));
-    let grpc_auth_service = grpc_auth::AuthService::new(banned_token_store);
+    // let addr = "[::0]:50051"
+    //     .parse()
+    //     .map_err(|_| tonic::Status::internal("Could not parse grpc address"));
+    // let banned_token_store = Arc::new(RwLock::new(HashsetBannedTokenStore::default()));
+    // let grpc_auth_service = grpc_auth::AuthService::new(banned_token_store);
 
-    println!("Running grpc server on: {:?}", addr);
-    tonic::transport::Server::builder()
-        .add_service(grpc_auth::AuthServer::new(grpc_auth_service))
-        .serve(addr.unwrap())
-        .await
+    // println!("Running grpc server on: {:?}", addr);
+    // tonic::transport::Server::builder()
+    //     .add_service(grpc_auth::AuthServer::new(grpc_auth_service))
+    //     .serve(addr.unwrap())
+    //     .await
+
+    Ok(())
 }
