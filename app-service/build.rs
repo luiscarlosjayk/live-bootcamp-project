9151cb25c955fd2d7307d9d86ee90185428916cf
Luis Carlos Osorio Jayk <luis.osorio@wizeline.com>
2024-06-09 10:10:28 -0600

    ci: disable compilation of protobuffers in build files

--- app-service/build.rs
+++ app-service/build.rs
@@ -4,7 +4,7 @@
  */
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_build::compile_protos("./proto/authentication.proto")?;
-    println!("cargo:rerun-if-changed=proto/authentication.proto");
+    // tonic_build::compile_protos("./proto/authentication.proto")?;
+    // println!("cargo:rerun-if-changed=proto/authentication.proto");
     Ok(())
 }
