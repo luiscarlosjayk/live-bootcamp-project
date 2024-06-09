c6bdbcd731c5517d084beb8bd8db46b8ebd4c564
Luis Carlos Osorio Jayk <luis.osorio@wizeline.com>
2024-06-09 10:15:21 -0600

    ci: disable compilation of protobuffers in build files

--- auth-service/src/services/grpc_auth.rs
+++ auth-service/src/services/grpc_auth.rs
@@ -1,40 +1,40 @@
-use crate::app_state::BannedTokenStoreType;
-use crate::utils::auth;
-use proto::auth_server::Auth;
-use proto::{StatusCode, VerifyTokenRequest, VerifyTokenResponse};
-use tonic::{Request, Response, Status};
+// use crate::app_state::BannedTokenStoreType;
+// use crate::utils::auth;
+// use proto::auth_server::Auth;
+// use proto::{StatusCode, VerifyTokenRequest, VerifyTokenResponse};
+// use tonic::{Request, Response, Status};
 
-pub mod proto {
-    tonic::include_proto!("authentication");
-}
+// pub mod proto {
+//     tonic::include_proto!("authentication");
+// }
 
-// Re-exporting
-pub use proto::auth_server::AuthServer;
+// // Re-exporting
+// pub use proto::auth_server::AuthServer;
 
-pub struct AuthService {
-    pub banned_token_store: BannedTokenStoreType,
-}
+// pub struct AuthService {
+//     pub banned_token_store: BannedTokenStoreType,
+// }
 
-impl AuthService {
-    pub fn new(banned_token_store: BannedTokenStoreType) -> Self {
-        Self { banned_token_store }
-    }
-}
+// impl AuthService {
+//     pub fn new(banned_token_store: BannedTokenStoreType) -> Self {
+//         Self { banned_token_store }
+//     }
+// }
 
-#[tonic::async_trait]
-impl Auth for AuthService {
-    async fn verify_token(
-        &self,
-        request: Request<VerifyTokenRequest>,
-    ) -> Result<Response<VerifyTokenResponse>, Status> {
-        println!("Got a grpc request: {:?}", request);
+// #[tonic::async_trait]
+// impl Auth for AuthService {
+//     async fn verify_token(
+//         &self,
+//         request: Request<VerifyTokenRequest>,
+//     ) -> Result<Response<VerifyTokenResponse>, Status> {
+//         println!("Got a grpc request: {:?}", request);
 
-        let token = request.into_inner().token;
-        match auth::validate_token(&token, self.banned_token_store.clone()).await {
-            Ok(_) => Ok(Response::new(VerifyTokenResponse {
-                status: StatusCode::Ok.into(),
-            })),
-            Err(_) => Err(Status::unauthenticated("The token is not valid")),
-        }
-    }
-}
+//         let token = request.into_inner().token;
+//         match auth::validate_token(&token, self.banned_token_store.clone()).await {
+//             Ok(_) => Ok(Response::new(VerifyTokenResponse {
+//                 status: StatusCode::Ok.into(),
+//             })),
+//             Err(_) => Err(Status::unauthenticated("The token is not valid")),
+//         }
+//     }
+// }
